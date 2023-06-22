f31f0baaccfd8ccc14eadc4b9519360d95f9dc39
NinerMG <maciej.gala@hotmail.com>
2023-06-22 11:37:30 +0200

    Dodanie metod i posortowanie kodu

--- src/pl/maciek/Wisielec.java
+++ src/pl/maciek/Wisielec.java
@@ -8,10 +8,64 @@ public class Wisielec {
     char[] zgadywanie;
     char[] wpisywaneSlowo;
     int lives = 3;
+    boolean shoot;
 
 
     public void start() {
 
+        intitialSetup();
+
+
+        //pętla while do kontynuwania gry
+        gameLoop();
+
+    }
+
+    private void gameLoop() {
+        while (!end()) {
+            checkingLetter();
+
+            checkingGameRules();
+        }
+    }
+
+    private void checkingGameRules() {
+        if (!shoot) {
+            System.out.println("Pudło! Straciłeś życie");
+            lives--;
+        }
+
+        if (lives == 0) {
+            System.out.println("Straciłeś wszystkie życia...przegrałeś");
+        }
+
+        if(Arrays.equals(wpisywaneSlowo, zgadywanie)) {
+            System.out.println("Gratulacje! Wygrałeś!");
+        }
+    }
+
+    private void checkingLetter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(wpisywaneSlowo);
+        System.out.println();
+        System.out.println("Pozostało żyć: " + lives);
+        System.out.println("Proszę podać zgadywaną literę");
+
+
+        char letter = scanner.nextLine().toLowerCase(Locale.ROOT).charAt(0);
+        shoot = false;
+
+        for (int i = 0; i < zgadywanie.length; i++) {
+            if (letter == zgadywanie[i]) {
+                wpisywaneSlowo[i] = letter;
+                System.out.println("Trafiłeś z literą " + letter);
+                shoot = true;
+            }
+
+        }
+    }
+
+    private void intitialSetup() {
         //utworzenie listy przykładowych słów
         ArrayList<String> words = new ArrayList<>(List.of("maciek", "dom", "klawiatura", "laptop", "tester"));
 
@@ -24,44 +78,6 @@ public class Wisielec {
         zgadywanie = wybraneSlowo.toCharArray();
         wpisywaneSlowo = new char[zgadywanie.length];
         Arrays.fill(wpisywaneSlowo, '_');
-        //System.out.println(wpisywaneSlowo);
-
-
-        //pętla while do kontynuwania gry
-        while (!end()) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(wpisywaneSlowo);
-            System.out.println();
-            System.out.println("Pozostało żyć: " + lives);
-            System.out.println("Proszę podać zgadywaną literę");
-
-
-            char letter = scanner.nextLine().toLowerCase(Locale.ROOT).charAt(0);
-            boolean shoot = false;
-
-            for (int i = 0; i < zgadywanie.length; i++) {
-                if (letter == zgadywanie[i]) {
-                    wpisywaneSlowo[i] = letter;
-                    System.out.println("Trafiłeś z literą " + letter);
-                    shoot = true;
-                }
-
-            }
-
-            if (!shoot) {
-                System.out.println("Pudło! Straciłeś życie");
-                lives--;
-            }
-
-            if (lives == 0) {
-                System.out.println("Straciłeś wszystkie życia...przegrałeś");
-            }
-
-            if(Arrays.equals(wpisywaneSlowo, zgadywanie)) {
-                System.out.println("Gratulacje! Wygrałeś!");
-            }
-        }
-
     }
 
     public boolean end() {
