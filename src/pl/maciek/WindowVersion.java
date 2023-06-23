5e2752d8e16568ea3d074634668e7b419863cb9e
NinerMG <maciej.gala@hotmail.com>
2023-06-23 16:08:25 +0200

    Dodanie prostego okienka GUI - wstępna wersja

---
+++ src/pl/maciek/WindowVersion.java
@@ -0,0 +1,18 @@
+package pl.maciek;
+
+import javax.swing.*;
+
+public class WindowVersion extends JFrame {
+
+    public WindowVersion() {
+        super("Wisielec (edycja Java)");
+        setSize(CommonConstants.FRAME_SIZE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+    }
+
+}
+
+
